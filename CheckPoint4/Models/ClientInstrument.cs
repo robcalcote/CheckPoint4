81a5c3b3dca24c7a1fc92007eb80dea4dbf3eae4
Rob Calcote <robcalcote@gmail.com>
2018-11-30 15:54:36 -0700

    removed NewClient model as it was not necessary, and added model within model to access client + instrument

---
+++ CheckPoint4/Models/ClientInstrument.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace CheckPoint4.Models
+{
+    public class ClientInstrument
+    {
+        public Client client { get; set; }
+        public Instrument instrument { get; set; }
+    }
+}
